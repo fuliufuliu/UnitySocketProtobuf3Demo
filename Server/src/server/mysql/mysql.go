0c6b373700e336f126e801f966abd39895790fb2
兴荣 <fuliufuliu@aliyun.com>
2020-07-21 16:17:47 +0800

    第一次调试成功

--- Server/src/server/mysql/mysql.go
+++ Server/src/server/mysql/mysql.go
@@ -13,8 +13,10 @@ var (
 
 func OpenDB() {
 	fmt.Println("mysqldb->open db")
-	db1, err := gorm.Open("mysql", "mike:123456@tcp(localhost:3306)/poker?parseTime=true")
+	//db1, err := gorm.Open("mysql", "mike:123456@tcp(localhost:3306)/poker?parseTime=true")
+	db1, err := gorm.Open("mysql", "root:123456@tcp(localhost:3306)/e3b?parseTime=true")
 	if err != nil {
+		fmt.Println(err)
 		panic("connect db error")
 	}
 	db = db1
